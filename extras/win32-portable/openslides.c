616985591308c34c84aa54934989b31b8534d1a1
Andy Kittner <andy.kittner@gmail.com>
2014-10-13 01:54:23 +0200

    port openslide.c to python3

--- extras/win32-portable/openslides.c
+++ extras/win32-portable/openslides.c
@@ -1,21 +1,22 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
+#include <wchar.h>
 
 #define _WIN32_LEAN_AND_MEAN
 #include <windows.h>
 
 #include <Python.h>
 
-#define PYTHON_DLL_PATH "\\Dlls\\python27.dll"
+#define PYTHON_DLL_PATH L"\\Dlls\\python34.dll"
 
 static void (*py_initialize)(void) = 0;
 static void (*py_finalize)(void) = 0;
-static void (*py_set_program_name)(char *) = 0;
-static void (*py_set_python_home)(char *) = 0;
+static void (*py_set_program_name)(wchar_t *) = 0;
+static void (*py_set_python_home)(wchar_t *) = 0;
 static int (*py_run_simple_string_flags)(const char *, PyCompilerFlags *) = 0;
-static void (*py_sys_set_argv_ex)(int, char **, int) = 0;
-static int (*py_main)(int, char **) = 0;
+static void (*py_sys_set_argv_ex)(int, wchar_t **, int) = 0;
+static int (*py_main)(int, wchar_t **) = 0;
 static int *py_ignore_environment_flag = 0;
 
 static const char *run_openslides_code =
@@ -27,11 +28,11 @@ static const char *run_openslides_code =
  *       this would trigger pythons search-path initialization
  *       But we need this to initialize PYTHONHOME before this happens
  */
-static char *
+static wchar_t *
 _get_module_name()
 {
     size_t size = 1;
-    char *name = NULL;
+    wchar_t *name = NULL;
     int i;
 
     /* a path > 40k would be insane, it is more likely something
@@ -40,9 +41,9 @@ _get_module_name()
     for (i = 0;i < 10; i++)
     {
 	DWORD res;
-	char *n;
+	wchar_t *n;
 
-	n = realloc(name, size);
+	n = realloc(name, size * sizeof(wchar_t));
 	if (!n)
 	{
 	    free(name);
@@ -50,7 +51,7 @@ _get_module_name()
 	}
 	name = n;
 
-	res = GetModuleFileNameA(NULL, name, size);
+	res = GetModuleFileNameW(NULL, name, size);
 	if (res != 0 && res < size)
 	{
 	    return name;
@@ -78,19 +79,19 @@ _get_module_name()
 }
 
 static void
-_fatal_error(const char *text)
+_fatal_error(const wchar_t *text)
 {
-    MessageBoxA(NULL, text, "Fatal error", MB_OK | MB_ICONERROR);
+    MessageBoxW(NULL, text, L"Fatal error", MB_OK | MB_ICONERROR);
     exit(1);
 }
 
 static void
-_fatal_error_fmt(const char *fmt, ...)
+_fatal_error_fmt(const wchar_t *fmt, ...)
 {
     int size = 512;
-    char *buf  = malloc(size);
+    wchar_t *buf  = malloc(size * sizeof(wchar_t));
     va_list args;
-    int bytes_written;
+    int chars_written;
 
     if (!buf)
 	abort();
@@ -98,15 +99,15 @@ _fatal_error_fmt(const char *fmt, ...)
     va_start(args, fmt);
     for (;;)
     {
-	bytes_written = vsnprintf(buf, size, fmt, args);
-	if (bytes_written > -1 && bytes_written < size)
+	chars_written = vswprintf(buf, size, fmt, args);
+	if (chars_written > -1 && chars_written < size)
 	    break;
-	else if (bytes_written > size)
-	    size = bytes_written + 1;
+	else if (chars_written > size)
+	    size = chars_written + 1;
 	else
 	    size *= 2;
 
-	buf = realloc(buf, size);
+	buf = realloc(buf, size * sizeof(wchar_t));
 	if (!buf)
 	    abort();
     }
@@ -120,42 +121,43 @@ _load_func(HMODULE module, const char *name)
 {
     void *address = GetProcAddress(module, name);
     if (!address)
-	_fatal_error_fmt("Failed to look up symbol %s", name);
+	_fatal_error_fmt(L"Failed to look up symbol %s", name);
     return address;
 }
 
 static void
-_load_python(const char *pyhome)
+_load_python(const wchar_t *pyhome)
 {
-    size_t pyhome_len = strlen(pyhome);
-    size_t size = pyhome_len + strlen(PYTHON_DLL_PATH) + 1;
-    char *buf = malloc(size);
+    size_t pyhome_len = wcslen(pyhome);
+    size_t dll_len = wcslen(PYTHON_DLL_PATH);
+    size_t size = pyhome_len + dll_len + 1;
+    wchar_t *buf = malloc(size * sizeof(wchar_t));
     HMODULE py_dll;
 
     if (!buf)
 	abort();
-    memcpy(buf, pyhome, pyhome_len);
-    memcpy(buf + pyhome_len, PYTHON_DLL_PATH, sizeof(PYTHON_DLL_PATH));
-    buf[size - 1] = '\0';
+    memcpy(buf, pyhome, pyhome_len * sizeof(wchar_t));
+    memcpy(buf + pyhome_len, PYTHON_DLL_PATH, dll_len * sizeof(wchar_t));
+    buf[size - 1] = L'\0';
 
-    py_dll = LoadLibrary(buf);
+    py_dll = LoadLibraryW(buf);
     if (!py_dll)
     {
 	DWORD error = GetLastError();
-	_fatal_error_fmt("Failed to load %s (error %d)", buf, error);
+	_fatal_error_fmt(L"Failed to load %s (error %d)", buf, error);
     }
 
     py_initialize = (void (*)(void))_load_func(py_dll, "Py_Initialize");
     py_finalize = (void (*)(void))_load_func(py_dll, "Py_Finalize");
-    py_set_program_name = (void (*)(char *))
+    py_set_program_name = (void (*)(wchar_t *))
 	_load_func(py_dll, "Py_SetProgramName");
-    py_set_python_home = (void (*)(char *))
+    py_set_python_home = (void (*)(wchar_t *))
 	_load_func(py_dll, "Py_SetPythonHome");
     py_run_simple_string_flags = (int (*)(const char *, PyCompilerFlags *))
 	_load_func(py_dll, "PyRun_SimpleStringFlags");
-    py_sys_set_argv_ex = (void (*)(int, char **, int))
+    py_sys_set_argv_ex = (void (*)(int, wchar_t **, int))
 	_load_func(py_dll, "PySys_SetArgvEx");
-    py_main = (int (*)(int, char **))_load_func(py_dll, "Py_Main");
+    py_main = (int (*)(int, wchar_t **))_load_func(py_dll, "Py_Main");
     py_ignore_environment_flag = (int *)
 	_load_func(py_dll, "Py_IgnoreEnvironmentFlag");
 }
@@ -164,43 +166,48 @@ static int
 _run()
 {
     if (py_run_simple_string_flags(run_openslides_code, NULL) != 0)
-	_fatal_error("Failed to execute openslides");
+	_fatal_error(L"Failed to execute openslides");
 
     return 0;
 }
 
 
+#if 0
 int WINAPI
 WinMain(HINSTANCE inst, HINSTANCE prev_inst, LPSTR cmdline, int show)
+#else
+int
+wmain(void)
+#endif
 {
     int returncode;
     int run_py_main = __argc > 1;
-    char *py_home, *sep = NULL;
+    wchar_t *py_home, *sep = NULL;
 
     py_home = _get_module_name();
     if (!py_home)
-	_fatal_error("Could not determine portable root directory");
+	_fatal_error(L"Could not determine portable root directory");
 
-    sep = strrchr(py_home, '\\');
+    sep = wcsrchr(py_home, L'\\');
     /* should always be the true */
     if (sep)
-	*sep = '\0';
+	*sep = L'\0';
 
     _load_python(py_home);
-    py_set_program_name(__argv[0]);
+    py_set_program_name(__wargv[0]);
     py_set_python_home(py_home);
     *py_ignore_environment_flag = 1;
 
     if (run_py_main)
     {
 	/* we where given extra arguments, behave like python.exe */
-	returncode =  py_main(__argc, __argv);
+	returncode =  py_main(__argc, __wargv);
     }
     else
     {
 	/* no arguments given => start openslides gui */
 	py_initialize();
-	py_sys_set_argv_ex(__argc, __argv, 0);
+	py_sys_set_argv_ex(__argc, __wargv, 0);
 
 	returncode = _run();
 	py_finalize();
